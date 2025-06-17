4fd0faa83d330f125367b55b0ad7018a44fd366b
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-06-18 03:22:24 +0500

    first commit

---
+++ js/register.js
@@ -0,0 +1,23 @@
+let elForm = document.querySelector(".register-form")
+
+elForm.addEventListener("submit", function(e){
+    e.preventDefault()
+     let data = {
+        id:passwords.length + 1,
+        username:e.target.username.value,
+        firstName:e.target.firstName.value,
+        lastName:e.target.lastName.value,
+        password:e.target.password.value,
+    }
+    passwords.push(data)
+    localStorage.setItem("passwords", JSON.stringify(passwords))
+    elForm.lastElementChild.innerHTML = `
+        <img class="w-[30px] h-[30px]  mx-auto" src="./images/loading-white.png" alt="loading" width="30" height="30" />
+    `
+    setTimeout(() => {
+        elForm.lastElementChild.innerHTML = `Sign up`
+        setTimeout(() => {
+            location.pathname = "/index.html"
+        },600)
+    },1000)
+})
